0d8fb050cde8967e622bfead4663edb27c7abe6f
URBACZKA IVAN <46192105+IVAN-URBACZKA@users.noreply.github.com>
2019-01-14 09:21:13 +0100

    Update main.js

--- main.js
+++ main.js
@@ -33,4 +33,32 @@ console.log(convertHTML("<>"));
 console.log(convertHTML("abc"));
 
 
+-----------------------------------------------------
+  
+  //Intermediate Algorithm Scripting: Sum All Odd Fibonacci Numbers
+
+  
+ function sumFibs(num) {
+
+    // create array
+    let arr = [1,1];
+
+    // addition with a loop for
+  for(i=1;i!=num;i++){
+        let actualNumber = arr[i];
+        let beforeNumber = arr [i-1];
+        let final = actualNumber + beforeNumber;
+        arr.push(final);
+    }
+    // filter item value if impair and less than num;
+    arr = arr.filter(item => item % 2 != 0 && item <= num);
+
+    // add impairs numbers
+    return arr.reduce(function(a,b){
+        return a + b;
+    })
+  }
+  
+  console.log(sumFibs(4));
+
 
